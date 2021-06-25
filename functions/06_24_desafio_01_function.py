6dfcb9a0284d871c83e5555dc1b4744b95a0eb59
Carlos Nicacio <carlosnicacio94@gmail.com>
2021-06-24 22:35:44 -0300

    Exercícios resolvidos - Aula 14 - Módulo 01 - Blue Edtech

---
+++ functions/06_24_desafio_01_function.py
@@ -0,0 +1,17 @@
+'''
+DESAFIO - Data com mês por extenso. Construa uma função que receba uma data no formato DD/MM/AAAA e devolva uma string no formato D de mesPorExtenso de AAAA. Opcionalmente, valide a data e retorne NULL caso a data seja inválida. Considere que Fevereiro tem 28 dias e que a cada 4 anos temos ano bisexto, sendo que nesses casos Fevereiro terá 29 dias.
+'''
+
+# def date(d, m, y):
+#     from datetime import date
+#     month = date(1900, m, 1).strftime('%B')
+#     return f'Dia {d} de {month} de {y}'
+
+# def menu():
+#     dd = int(input('Digite o dia: '))
+#     mm = int(input('Digite o mês: '))
+#     yyyy = int(input('Digite o ano: '))
+#     return date(dd, mm, yyyy)
+
+# print(menu())
+
